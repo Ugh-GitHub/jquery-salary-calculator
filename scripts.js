f3510185d9b9c4a1725e90ea523e3119bf4f20a1
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-04 09:02:29 -0500

    instructions added to files

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 console.log("hello world");
 $(document).ready(console.log("hello jquery"));
+
+// object to hold:
+// employee first name, 
+// last name, 
+// ID number, 
+// job title, 
+// annual salary
+
+// Collect/store clientInformation
+// calculate monthly Costs
+// append information to the DOM and clear the input fields
+// If the total monthly cost exceeds $20,000, 
+// add a red background color to the total monthly cost.
