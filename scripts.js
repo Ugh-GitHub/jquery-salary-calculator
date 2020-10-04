21e21203fb932929fab0ecd0d26ef8c9cbaee7d3
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-04 11:23:57 -0500

    error input testing draft 1

--- scripts.js
+++ scripts.js
@@ -17,7 +17,100 @@ let empList = []; // Employee list declaration
 <input type="text" id="annualSalary" class="input" placeholder="Employee Annual Salary"> */}
 
 function onReady() {
-    $( '#submitEmployee' ).on('click',employeeObj);
+    $( '#submitEmployee' ).on('click',processInput);
+}
+
+function processInput() {
+    // input testing goes here
+    // testInput();
+    if (testInput()) {
+        
+    
+    // function to create a new table row, insert the input
+    
+
+    // function to clear the previous input fields
+    clearFields();
+
+    // function to calculate monthly costs
+    calculateCosts();
+
+    // function to append monthly costs
+
+    // function to test monthly costs exceeding $20,000 
+    // if so, appending id to prompt CSS to turn text red
+    }
+}
+
+function testInput() {
+
+    let errorTester = true;
+    // need to test that the ID number is a number
+    // need to test that Annual Salary is a number
+    // need to test that all fields are filled
+
+    errorTester = testFilled();
+    
+    // assign input to variables
+    let idNumber = $( '#idNumber').val();
+    let annualSalary = $( '#annualSalary').val();
+
+    // test idNumber is a number
+    if (isNaN(Number(idNumber))) {
+
+        // if NaN prompt the user the input is invalid and return false
+        console.log('idNumber is NaN');
+        $('#idNumber').val('');
+        $('#idNumber').attr('placeholder','*Needs Number*');
+        //$('#idNumber').addClass('errorInput');
+        errorTester = false;
+    }
+
+    // test annual salary is a number
+    if (isNaN(Number(annualSalary))) {
+
+        // if NaN prompt the user the input is invalid and return false
+        console.log('annualSalary is NaN');
+        $('#annualSalary').val('');
+        $('#annualSalary').attr('placeholder','*Needs Number*');
+        //$('#annualSalary').addClass('errorInput');
+        errorTester = false;
+    }
+
+    // return true otherwise
+    return errorTester;
+}
+
+function testFilled() {
+    let errorTester = true;
+
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $( '#idNumber').val();
+    let jobTitle = $( '#jobTitle').val();
+    let annualSalary = $( '#annualSalary').val();
+
+    if (firstName.length == 0) {
+        $('#firstName').toggleClass('errorInput');
+        errorTester = false;
+    }
+    if (lastName.length == 0) {
+        $('#lastName').toggleClass('errorInput');
+        errorTester = false;
+    }
+    if (idNumber.length == 0) {
+        $('#idNumber').toggleClass('errorInput');
+        errorTester = false;
+    }
+    if (jobTitle.length == 0) {
+        $('#jobTitle').toggleClass('errorInput');
+        errorTester = false;
+    }
+    if (annualSalary.length == 0) {
+        $('#annualSalary').toggleClass('errorInput');
+        errorTester = false;
+    }
+    return errorTester;
 }
 
 
@@ -30,24 +123,11 @@ function employeeObj() {
         jobTitle: $( '#jobTitle').val(),
         annualSalary: $( '#annualSalary').val()
     };
-    clearFields()
+    
     empList.push(tempObj);
     console.log('in employee obj');
-    updateTableDOM(tempObj);
     return tempObj;
 }
-
-function clearFields() {
-    // Clear the fields
-    console.log('in clearFields');
-    $( '#firstName').val('');
-    $( '#lastName').val('');
-    $( '#idNumber').val('');
-    $( '#jobTitle').val('');
-    $( '#annualSalary').val('');
-    return true;
-}
-
 
 function updateTableDOM(empObj) {
     // "employeeInfoTable"
@@ -69,8 +149,23 @@ function updateTableDOM(empObj) {
     return true;
 }
 
-// Collect/store clientInformation
-// calculate monthly Costs
-// append information to the DOM and clear the input fields
-// If the total monthly cost exceeds $20,000, 
-// add a red background color to the total monthly cost.
+function clearFields() {
+    // Clear the fields
+    console.log('in clearFields');
+    $( '#firstName').val('');
+    $( '#lastName').val('');
+    $( '#idNumber').val('');
+    $( '#jobTitle').val('');
+    $( '#annualSalary').val('');
+    return true;
+}
+
+function calculateCosts() {
+    let sum = 0;
+    for (let i = 0; i < empList.length; i++) {
+        sum += empList[i];
+    }
+    // put a text function here to add the info to the dom
+    return sum;
+}
+
